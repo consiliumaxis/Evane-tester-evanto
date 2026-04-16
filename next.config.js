f756b1afd38a3be6d4a16c2590a67edc5b558183
BigDealneon <kneon0207@gmail.com>
2026-04-16 17:01:57 +0800

    Configure Next.js for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  basePath: '/Evane-tester',
+  assetPrefix: '/Evane-tester/',
+  trailingSlash: true,
+  exportPathMap: async (defaultPathMap) => {
+    return defaultPathMap
+  }
+}
